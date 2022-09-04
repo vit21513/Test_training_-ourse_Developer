ffe1c123ab8207e9d3dcb0af5d6b5be258e845be
«vitaly» <vit21513@yandex.ru>
2022-09-04 13:26:49 +0300

    add taskC#

---
+++ taskC#/Program.cs
@@ -0,0 +1,24 @@
+﻿
+void СhoiceArray(string[] array, string[] secondArray)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            secondArray[i] = array[i];
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]} ");
+    }
+}
+Console.Clear();
+string[] array = { "hello", "2", "world", ":-)" };
+string[] newarray = new string[array.Length];
+СhoiceArray(array, newarray);
+PrintArray(newarray);
